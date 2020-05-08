56dbb7925f002fe13a5f2610352384b8ed1a400a
Diego Alonso <diego98_alonso@hotmail.com>
2020-05-08 10:08:42 -0500

    Se agregaron las entidades distritos y sedes, pero se tiene que analizar detalladamente las claves foraneas de las entidades sedes y distritos

---
+++ src/main/java/com/acme/blogging/model/Districts.java
@@ -0,0 +1,20 @@
+package com.acme.blogging.model;
+
+import lombok.Getter;
+import lombok.Setter;
+
+import javax.persistence.*;
+
+@Entity
+@Table(name = "districts")
+@Getter
+@Setter
+public class Districts {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "ID")
+    private long id;
+
+    @Column(name = "Districts Name",length = 30,nullable = false)
+    private String districtname;
+}
