1ea08f0c015db601ccfd34c4d2cb69792cf7fbf3
Diego Alonso <diego98_alonso@hotmail.com>
2020-05-08 01:33:40 -0500

    Entidad clinicas(falta su clave foranea)

--- src/main/java/com/acme/blogging/model/Clinic.java
+++ src/main/java/com/acme/blogging/model/Clinic.java
@@ -1,9 +1,14 @@
 package com.acme.blogging.model;
 
+import lombok.Getter;
+import lombok.Setter;
+
 import javax.persistence.*;
 
 @Entity
-@Table(name = "clinics")
+@Table(name = "clinic")
+@Getter
+@Setter
 public class Clinic  {
 
     @Id
--- src/main/java/com/acme/blogging/model/Patients.java
+++ src/main/java/com/acme/blogging/model/Patients.java
@@ -1,4 +1,5 @@
 package com.acme.blogging.model;
 
 public class Patients {
+
 }
