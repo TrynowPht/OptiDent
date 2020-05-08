f088bb70e36ad836cd1ed309fe2df5e0fef007e3
Lenovo <christian_joseph7@hotmail.com>
2020-05-08 13:05:33 -0500

    Patients so

--- src/main/java/com/acme/blogging/model/Patients.java
+++ src/main/java/com/acme/blogging/model/Patients.java
@@ -2,6 +2,7 @@ package com.acme.blogging.model;
 
 import lombok.Getter;
 import lombok.Setter;
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
 
 import javax.persistence.*;
 import javax.validation.constraints.NotBlank;
@@ -9,9 +10,10 @@ import javax.validation.constraints.NotNull;
 import javax.validation.constraints.Size;
 
 @Entity
-//Table(name="patients")
+@Table(name="patients")
 @Getter
 @Setter
+@EntityListeners(AuditingEntityListener.class)
 public class Patients extends AuditModel{
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
@@ -39,6 +41,34 @@ public class Patients extends AuditModel{
 
     @NotNull
     @NotBlank
-    @Size(max = 100)
     private Long  phone;
+
+    @NotNull
+    @NotBlank
+    @Size(max = 100)
+    private String  ocupation;
+
+    @NotNull
+    @NotBlank
+    @Size(max = 100)
+    private String  civilState;
+
+    @NotNull
+    @NotBlank
+    @Size(max = 100)
+    @Column(nullable=false,updatable=false)
+    @Temporal (TemporalType.TIMESTAMP)
+    private String birth;
+
+    @NotNull
+    @NotBlank
+    @Size(max = 100)
+    @Column(nullable=false,updatable=false)
+    @Temporal (TemporalType.TIMESTAMP)
+    private String inscription;
+
+    @NotNull
+    @NotBlank
+    @Column(nullable=false)
+    private String email;
 }
